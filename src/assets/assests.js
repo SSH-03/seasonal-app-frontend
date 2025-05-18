9d9e3e1afd03ad576483e5b087a47896f3c3ca60
Midhun <midhunnagasaicse@gmail.com>
2025-05-18 22:23:42 +0530

    Basic Structure of application created (directories, pages)

---
+++ src/assets/assests.js
@@ -0,0 +1,32 @@
+import SSH_Logo from "./SSH_Logo.jpg"
+
+export const assets = {
+    SSH_Logo
+}
+
+export const products = [
+  {
+    _id: "aaaa",
+    name: "Kites",
+    description: "Used for flying",
+    price: 50,
+    image: [SSH_Logo],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "aaaaaa",
+    name: "Kite Lamps",
+    description: "Used for flying at Night",
+    price: 250,
+    image: [SSH_Logo, SSH_Logo, SSH_Logo,SSH_Logo],
+    category: "Men",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+];
